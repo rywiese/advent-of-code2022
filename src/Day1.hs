2bed88d6222cb628b5f56fa4842ae29b239c9614
Ry Wiese <ry.wiese@gmail.com>
2022-12-18 21:03:44 -0600

    refactor

--- src/Day1.hs
+++ src/Day1.hs
@@ -3,43 +3,45 @@ module Day1 where
 import Control.Exception
 import System.FilePath ((</>))
 import System.IO
-import Data.Foldable (maximumBy)
 
-type Elf = Int
 type Calories = Int
 
-solution :: IO (Elf, Calories)
-solution = do
-    elfMap <- readInputFile day1InputFile
-    return (findElfWithMostCalories elfMap)
+data Elf = Elf
+    { index :: Int
+    , caloriesList :: [Int]
+    }
 
-compareElvesCalories:: (Elf, [Calories]) -> (Elf, [Calories]) -> Ordering
-compareElvesCalories (_, calories1) (_, calories2) = compare (sum calories1) (sum calories2)
+instance Eq Elf where
+    elf1 == elf2 = totalCalories elf1 == totalCalories elf2
 
-findElfWithMostCalories :: [(Elf, [Calories])] -> (Elf, Calories)
-findElfWithMostCalories elfMap = let (elf, caloriesList) = findElfWithMostCalories' elfMap in (elf, sum caloriesList)
+instance Ord Elf where
+    compare elf1 elf2 = Prelude.compare (totalCalories elf1) (totalCalories elf2)
 
-findElfWithMostCalories' :: [(Elf, [Calories])] -> (Elf, [Calories])
-findElfWithMostCalories' = maximumBy compareElvesCalories
+totalCalories :: Elf -> Int
+totalCalories (Elf _ caloriesList) = sum caloriesList
 
 day1InputFile :: FilePath
 day1InputFile = "/Users/ry/advent-of-code/advent-of-code2022/res" </> "day1.in"
 
-readInputFile :: FilePath -> IO [(Elf, [Calories])]
-readInputFile filePath = do
-    elfHandle <- System.IO.openFile filePath ReadMode
-    readElfMapList elfHandle 1
+day1 :: IO ()
+day1 = do
+    elfHandle <- System.IO.openFile day1InputFile ReadMode
+    elves <- readElves elfHandle
+    System.IO.print (totalCalories (maximum elves))
 
-readElfMapList :: Handle -> Elf -> IO [(Elf, [Calories])]
-readElfMapList elfHandle index = do
+readElves :: Handle -> IO [Elf]
+readElves elfHandle = readElves' elfHandle 0
+
+readElves' :: Handle -> Int -> IO [Elf]
+readElves' elfHandle index = do
     caloriesList <- readCaloriesList elfHandle
     case caloriesList of
         [] -> return []
         _ -> do
-            rest <- readElfMapList elfHandle (index + 1)
-            return ((index, caloriesList) : rest)
+            rest <- readElves' elfHandle (index + 1)
+            return (Elf { index, caloriesList } : rest)
 
-readCaloriesList :: Handle -> IO [Calories]
+readCaloriesList :: Handle -> IO [Int]
 readCaloriesList elfHandle = do
     lineEither <- Control.Exception.try (System.IO.hGetLine elfHandle)
     case lineEither of
