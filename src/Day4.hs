866f3e14df24794bf1694589fdd3318d29bf7174
Ry Wiese <ry.wiese@gmail.com>
2022-12-21 20:32:49 -0600

    Day 4

--- src/Day4.hs
+++ src/Day4.hs
@@ -19,12 +19,20 @@ leftFullyContainsRight
 oneFullyContainsTheOther :: SectionAssignment -> SectionAssignment -> Bool
 oneFullyContainsTheOther ass1 ass2 = leftFullyContainsRight ass1 ass2 || leftFullyContainsRight ass2 ass1
 
+hasOverlap :: SectionAssignment -> SectionAssignment -> Bool
+hasOverlap
+    SectionAssignment { mini=min1, maxi=max1 }
+    SectionAssignment { mini=min2, maxi=max2 } = min2 <= max1 && min1 <= max2
+
 parseLine :: String -> (SectionAssignment, SectionAssignment)
 parseLine line = let [min1, max1, min2, max2] = map read (splitOneOf "-," line) in
     (SectionAssignment min1 max1, SectionAssignment min2 max2)
 
-numFullContainments :: [(SectionAssignment, SectionAssignment)] -> Int
-numFullContainments = length . filter id . map (uncurry oneFullyContainsTheOther)
+numPairsWhere :: (a -> a -> Bool) -> [(a, a)] -> Int
+numPairsWhere f = length . filter id . map (uncurry f)
 
 day4part1 :: IO ()
-day4part1 = parseAndSolve 4 parseLine numFullContainments
+day4part1 = parseAndSolve 4 parseLine (numPairsWhere oneFullyContainsTheOther)
+
+day4part2 :: IO ()
+day4part2 = parseAndSolve 4 parseLine (numPairsWhere hasOverlap)
