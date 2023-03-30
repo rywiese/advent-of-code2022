ced567d8d38a892a8b7feed33b4e633e906bc088
Ry Wiese <ry.wiese@gmail.com>
2023-03-30 16:25:19 -0500

    Day 8 part 2

--- src/Day8.hs
+++ src/Day8.hs
@@ -2,9 +2,13 @@ module Day8 where
 
 import IOHelpers
 import Data.Char (digitToInt)
+import Data.Foldable (maximumBy)
 
 crossSection :: Int -> [[a]] -> [a]
 crossSection index = map (!! index)
+
+quadToList :: (a, a, a, a) -> [a]
+quadToList (w, x, y, z) = [w, x, y, z]
 
 data Tree = Tree
     { row :: Int
@@ -18,24 +22,35 @@ instance Eq Tree where
 instance Ord Tree where
     tree1 <= tree2 = height tree1 <= height tree2
 
-visibleInGrid :: [[Tree]] -> Tree -> Bool
-visibleInGrid forest tree = visibleInRow (forest !! row tree) (column tree) || visibleInRow (crossSection (column tree) forest) (row tree)
+neighbors :: [[Tree]] -> Tree -> ([Tree], [Tree], [Tree], [Tree])
+neighbors forest tree = let (upNeighbors, _:downNeighbors) = splitAt (row tree) (crossSection (column tree) forest) in
+    let (leftNeighbors, _:rightNeighbors) = splitAt (column tree) (forest !! row tree) in
+        (reverse upNeighbors, downNeighbors, reverse leftNeighbors, rightNeighbors)
 
-visibleInRow :: [Tree] -> Int -> Bool
-visibleInRow trees index = case splitAt index trees of
-    ([], _) -> True
-    (_, []) -> False
-    (_, [_]) -> True
-    (left, h:right) -> all (< h) left || all (< h) right
+visibleInForest :: [[Tree]] -> Tree -> Bool
+visibleInForest forest tree = foldl (\acc -> (acc ||) . all (< tree)) False $ quadToList $ neighbors forest tree
 
 numberOfTreesVisible :: [[Tree]] -> Int
-numberOfTreesVisible grid = (length . filter (visibleInGrid grid) . concat) grid
+numberOfTreesVisible forest = (length . filter (visibleInForest forest) . concat) forest
 
 day8part1 :: IO ()
 day8part1 = readAndSolve 8 (numberOfTreesVisible . parseForest)
 
+viewingDistance :: Tree -> [Tree] -> Int
+viewingDistance _ [] = 0
+viewingDistance tree (h:t) = if height tree <= height h then 1 else 1 + viewingDistance tree t
+
+scenicScore :: [[Tree]] -> Tree -> Int
+scenicScore forest tree = foldl (\acc neighbs -> acc * viewingDistance tree neighbs) 1 (quadToList $ neighbors forest tree)
+
+treeWithMaxScenicScore :: [[Tree]] -> Tree
+treeWithMaxScenicScore forest = (maximumBy (\tree1 tree2 -> compare (scenicScore forest tree1) (scenicScore forest tree2)) . concat) forest
+
+maxScenicScore :: [[Tree]] -> Int
+maxScenicScore forest = scenicScore forest $ treeWithMaxScenicScore forest
+
 day8part2 :: IO ()
-day8part2 = readAndSolve 8 id
+day8part2 = readAndSolve 8 (maxScenicScore . parseForest)
 
 parseForest :: [String] -> [[Tree]]
 parseForest = heightGridToForest . parseHeightGrid
