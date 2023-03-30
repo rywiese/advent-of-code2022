60bd0f57edbccac0ffa7a8c9a9a35943f6ba7d79
Ry Wiese <ry.wiese@gmail.com>
2023-03-29 22:00:51 -0500

    Day7

--- src/IOHelpers.hs
+++ src/IOHelpers.hs
@@ -4,6 +4,12 @@ import Control.Exception
 import System.FilePath ((</>))
 import System.IO
 
+-- parseAndSolve probably should be written in terms of readAndSolve, not the other way around
+-- TODO: add tests for previous days to ensure no regressions (and to learn Haskell tests :)),
+-- then refactor at will
+readAndSolve :: Show a => Int -> ([String] -> a) -> IO ()
+readAndSolve day = parseAndSolve day id
+
 parseAndSolve :: Show b => Int -> (String -> a) -> ([a] -> b) -> IO ()
 parseAndSolve day lineParser solve = do
     handle <- System.IO.openFile (inputFileForDay day) ReadMode
