9986f81c5b68d3f4c214de32736dcaaec29318b6
Ry Wiese <ry.wiese@gmail.com>
2023-03-31 11:22:54 -0500

    Day 9 part 2

--- src/Day9.hs
+++ src/Day9.hs
@@ -3,9 +3,12 @@ module Day9 where
 import IOHelpers
 import Data.Set (fromList)
 
+composeNTimes :: (a -> a) -> Int -> (a -> a)
+composeNTimes f n = foldl (.) id (replicate n f)
+
 data Direction = C | N | NE | E | SE | S | SW | W | NW deriving (Show)
 
-data HeadMovement = U | D | L | R deriving (Show)
+type HeadMovement = Direction
 
 type TailMovement = Direction
 
@@ -14,42 +17,96 @@ type TailPosition = Direction
 type Coordinates = (Int, Int)
 
 follow :: TailPosition -> HeadMovement -> (Maybe TailMovement, TailPosition)
-follow C U = (Nothing, S)
-follow C D = (Nothing, N)
-follow C L = (Nothing, E)
-follow C R = (Nothing, W)
-follow N U = (Nothing, C)
-follow N D = (Just S, N)
-follow N L = (Nothing, NE)
-follow N R = (Nothing, NW)
-follow NE U = (Nothing, E)
-follow NE D = (Just SW, N)
-follow NE L = (Just SW, E)
-follow NE R = (Nothing, N)
-follow E U = (Nothing, SE)
-follow E D = (Nothing, NE)
-follow E L = (Just W, E)
-follow E R = (Nothing, C)
-follow SE U = (Just NW, S)
-follow SE D = (Nothing, E)
-follow SE L = (Just NW, E)
-follow SE R = (Nothing, S)
-follow S U = (Just N, S)
-follow S D = (Nothing, C)
-follow S L = (Nothing, SE)
-follow S R = (Nothing, SW)
-follow SW U = (Just NE, S)
-follow SW D = (Nothing, W)
-follow SW L = (Nothing, S)
-follow SW R = (Just NE, W)
-follow W U = (Nothing, SW)
-follow W D = (Nothing, NW)
-follow W L = (Nothing, C)
-follow W R = (Just E, W)
-follow NW U = (Nothing, W)
-follow NW D = (Just SE, N)
-follow NW L = (Nothing, N)
-follow NW R = (Just SE, W)
+
+follow C C = (Nothing, C)
+follow C N = (Nothing, S)
+follow C NE = (Nothing, SW)
+follow C E = (Nothing, W)
+follow C SE = (Nothing, NW)
+follow C S = (Nothing, N)
+follow C SW = (Nothing, NE)
+follow C W = (Nothing, E)
+follow C NW = (Nothing, SE)
+
+follow N C = (Nothing, N)
+follow N N = (Nothing, C)
+follow N NE = (Nothing, W)
+follow N E = (Nothing, NW)
+follow N SE = (Just SE, N)
+follow N S = (Just S, N)
+follow N SW = (Just SW, N)
+follow N W = (Nothing, NE)
+follow N NW = (Nothing, E)
+
+follow NE C = (Nothing, NE)
+follow NE N = (Nothing, E)
+follow NE NE = (Nothing, C)
+follow NE E = (Nothing, N)
+follow NE SE = (Just S, N)
+follow NE S = (Just SW, N)
+follow NE SW = (Just SW, NE)
+follow NE W = (Just SW, E)
+follow NE NW = (Just W, E)
+
+follow E C = (Nothing, E)
+follow E N = (Nothing, SE)
+follow E NE = (Nothing, S)
+follow E E = (Nothing, C)
+follow E SE = (Nothing, N)
+follow E S = (Nothing, NE)
+follow E SW = (Just SW, E)
+follow E W = (Just W, E)
+follow E NW = (Just NW, E)
+
+follow SE C = (Nothing, SE)
+follow SE N = (Just NW, S)
+follow SE NE = (Just N, S)
+follow SE E = (Nothing, S)
+follow SE SE = (Nothing, C)
+follow SE S = (Nothing, E)
+follow SE SW = (Just W, E)
+follow SE W = (Just NW, E)
+follow SE NW = (Just NW, SE)
+
+follow S C = (Nothing, S)
+follow S N = (Just N, S)
+follow S NE = (Just NE, S)
+follow S E = (Nothing, SW)
+follow S SE = (Nothing, W)
+follow S S = (Nothing, C)
+follow S SW = (Nothing, E)
+follow S W = (Nothing, SE)
+follow S NW = (Just NW, S)
+
+follow SW C = (Nothing, SW)
+follow SW N = (Just NE, S)
+follow SW NE = (Just NE, SW)
+follow SW E = (Just NE, W)
+follow SW SE = (Just E, W)
+follow SW S = (Nothing, W)
+follow SW SW = (Nothing, C)
+follow SW W = (Nothing, S)
+follow SW NW = (Just N, S)
+
+follow W C = (Nothing, W)
+follow W N = (Nothing, SW)
+follow W NE = (Just NE, W)
+follow W E = (Just E, W)
+follow W SE = (Just SE, W)
+follow W S = (Nothing, NW)
+follow W SW = (Nothing, N)
+follow W W = (Nothing, C)
+follow W NW = (Nothing, S)
+
+follow NW C = (Nothing, NW)
+follow NW N = (Nothing, W)
+follow NW NE = (Just E, W)
+follow NW E = (Just SE, W)
+follow NW SE = (Just SE, NW)
+follow NW S = (Just SE, N)
+follow NW SW = (Just S, N)
+follow NW W = (Nothing, N)
+follow NW NW = (Nothing, C)
 
 tailMovements :: [HeadMovement] -> [TailMovement]
 tailMovements = reverse . snd . foldl accumulateTailMovements (C, [])
@@ -67,7 +124,7 @@ newCoordinatesFor (x, y) E = (x + 1, y)
 newCoordinatesFor (x, y) SE = (x + 1, y - 1)
 newCoordinatesFor (x, y) S = (x, y - 1)
 newCoordinatesFor (x, y) SW = (x - 1, y - 1)
-newCoordinatesFor (x, y) W = (x -1 , y)
+newCoordinatesFor (x, y) W = (x - 1 , y)
 newCoordinatesFor (x, y) NW = (x - 1, y + 1)
 
 appendNewCoordinatesFor :: [Coordinates] -> TailMovement -> [Coordinates]
@@ -77,11 +134,14 @@ appendNewCoordinatesFor (h:t) tailMovement = newCoordinatesFor h tailMovement:h:
 buildCoordinatesFrom :: [TailMovement] -> [Coordinates]
 buildCoordinatesFrom = foldl appendNewCoordinatesFor [(0, 0)]
 
-numUniqueCoordinates :: [HeadMovement] -> Int
-numUniqueCoordinates = length . fromList . buildCoordinatesFrom . tailMovements
+numUniqueCoordinates :: Int -> [HeadMovement] -> Int
+numUniqueCoordinates numKnots = length . fromList . buildCoordinatesFrom . composeNTimes tailMovements (numKnots - 1)
 
 day9part1 :: IO ()
-day9part1 = readAndSolve 9 (numUniqueCoordinates . parseSteps)
+day9part1 = readAndSolve 9 (numUniqueCoordinates 2 . parseSteps)
+
+day9part2 :: IO ()
+day9part2 = readAndSolve 9 (numUniqueCoordinates 10 . parseSteps)
 
 data Instruction = Instruction
     { headMovement :: HeadMovement
@@ -95,10 +155,10 @@ expandSteps :: Instruction -> [HeadMovement]
 expandSteps instruction = replicate (steps instruction) (headMovement instruction)
 
 parseInstruction :: String -> Instruction
-parseInstruction (headMovement:' ':steps) = Instruction { headMovement = parseHeadMovement headMovement, steps = read steps}
+parseInstruction (headMovement:' ':steps) = Instruction { headMovement = parseHeadMovement headMovement, steps = read steps }
 
 parseHeadMovement :: Char -> HeadMovement
-parseHeadMovement 'U' = U
-parseHeadMovement 'D' = D
-parseHeadMovement 'L' = L
-parseHeadMovement 'R' = R
+parseHeadMovement 'U' = N
+parseHeadMovement 'D' = S
+parseHeadMovement 'L' = W
+parseHeadMovement 'R' = E
